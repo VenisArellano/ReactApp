806ec4ad4bffd429df3e34a0a44c756ac4f6929b
Venis Arellano <venisarellano@gmail.com>
2021-08-25 23:54:12 -0300

    Desafio clase 12 - Arellano

---
+++ src/components/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoLDBFr6zaM9dVZ2ARd8wCpmt1xn8DDaw",
+  authDomain: "coderhouse-tienda-2021.firebaseapp.com",
+  projectId: "coderhouse-tienda-2021",
+  storageBucket: "coderhouse-tienda-2021.appspot.com",
+  messagingSenderId: "8061326823",
+  appId: "1:8061326823:web:45099d480e8dce333d5942"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
